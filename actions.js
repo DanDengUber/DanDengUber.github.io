c6c7057b2064d540532e9109bceba6655b983058
DanDengUber <54044721+DanDengUber@users.noreply.github.com>
2024-06-25 15:34:34 -0700

    send open web action

--- actions.js
+++ actions.js
@@ -29,3 +29,35 @@ function sendActionToAndroid() {
         console.log("AndroidBridge is not available");
     }
 }
+
+function sendOpenWebActionToAndroid() {
+    var payload = {
+        "type": "membership_action_event",
+        "messageId": "083d5b55-1b58-4df0-afe4-5f3751b65df0",
+        "requestId": "35329fb7-af3c-48c1-b2b2-12c9a8ac1197",
+        "payload": {
+            "membershipAction": {
+                "data": {
+                    "type": "openWeb",
+                    "openWeb": {
+                        "url": "https://services.sheerid.com/rest/v2/verification/667b42688e764b6dae6e023c/step/sso",
+                        "webviewConfig": {
+                            "type": "presidio",
+                            "presidio": {
+                                "webviewType": "EMBEDDED_WEB_VIEW"
+                            }
+                        }
+                    }
+                },
+                "actionSource": "NON_MEMBER_STUDENT_PLAN_MONTHLY"
+            }
+        }
+    };
+
+    if (window.MembershipPresidioWebBridge) {
+        console.log("AndroidBridge Msg sent SUCCESS!!");
+        window.MembershipPresidioWebBridge.onMembershipAction(JSON.stringify(payload));
+    } else {
+        console.log("AndroidBridge is not available");
+    }
+}
